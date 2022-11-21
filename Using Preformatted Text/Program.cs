7db2b082b29fa2ec65685562f68675146259e664
Okafornta <hillaryonia@gmail.com>
2022-11-21 10:55:42 +0100

    Add project files.

---
+++ Using Preformatted Text/Program.cs
@@ -0,0 +1,20 @@
+﻿//TASK: Writing a program that display multiline text
+// Multiline text
+
+Console.WriteLine(@"
+My name is Hillary Chinemerem Okafornta,
+I graduated from University of Nigeria Nsukka,
+where i studied Banking and Finance, Faculty of 
+Business Administration,
+I am now Doing my Internship with FedEx Nigeria as a
+Software Developer.");
+
+// Waiting for Enter
+Console.ReadLine();
+
+/* NOTE:
+ * 
+ *  The at (@) sign also switches off escape sequences. That is why you might find it useful
+    when dealing with file paths in Windows (mentioned earlier); in that case, you do not
+    have to double each backslash.
+ */
